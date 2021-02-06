c620f6ec86df1b8923cc37b2f7e99632b3075353
Zeben <yusnebez@gmail.com>
2021-02-06 14:16:04 +0000

    conmongo4

--- api/utils/index.js
+++ api/utils/index.js
@@ -6,7 +6,7 @@ function authUser (req, res, next) {
     if (!req.headers.token) {
       res.status(403).json({ error: 'No Token found' })
     } else {
-      jwt.verify(req.headers.token, 'secret', (err, token) => {
+      jwt.verify(req.headers.token, process.env.SECRET, (err, token) => {
         if (err) { res.status(403).json({ error: 'Token not valid' }) }
   
         teacherModel.findOne({ email: token.email })
