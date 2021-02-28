9f39843aeed3953ab5495e795892a09614fcef21
Zeben <yusnebez@gmail.com>
2021-02-28 00:06:04 +0000

    changes

--- public/signup.js
+++ public/signup.js
@@ -1,10 +1,10 @@
 const api = axios.create({
-    baseURL:"https://tecnoeducate.herokuapp.com/api",
+    baseURL:"https://tecnoeducate.herokuapp.com/",
     timeout: 2000
 })
 
 document.getElementById('signup').addEventListener("click", function(){
-    axios.post('/auth/signup',{
+    axios.post('api/auth/signup',{
     name: document.getElementById('signup_name').value,
     email: document.getElementById('signup_email').value,
     password: document.getElementById('signup_password').value
